77a36a3c926da30f04f3bd44b9011c08cecc9fe5
manojkumar <mancse@gmail.com>
2015-06-21 00:50:50 +0530

    adspace-app

---
+++ adspace_app/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name adspaceAppApp
+ * @description
+ * # adspaceAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('adspaceAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ adspace_app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adspaceAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adspaceAppApp
+ */
+angular.module('adspaceAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
